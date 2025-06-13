ee612896eaa08925c2f990f747a21cdda2e0e3f6
MMoina14 <miguelamoina@gmail.com>
2025-06-12 21:16:51 -0400

    MY0lab

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 //Write your code here
 
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
+
+
+function logAttendeeName(attendee) {
+  console.log("Attendee Name:", attendee.name);
+}
+
+
+function logTicketPrice(attendee) {
+  console.log("Ticket Price:", attendee.ticketPrice);
+}
+
+
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+  console.log(`Ticket type updated to: ${attendee.ticketType}`);
+}
+
+
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+  console.log(`Ticket price updated to: $${attendee.ticketPrice}`);
+}
+
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+  console.log("Event property removed.");
+}
+
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+  console.log("Checked-in property added.");
+}
+
 
 
 //Needed for the tests to work. Don't modify
